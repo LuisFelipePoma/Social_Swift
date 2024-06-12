cea3e8f1dfe244ecf13a755b5bfd8de721fb0488
Andre <andrepilcoch@gmail.com>
2024-06-12 16:33:08 -0500

    working information modification

--- api/src/main/java/com/socialswift/api/model/dto/WorkingInformationResponseDTO.java
+++ api/src/main/java/com/socialswift/api/model/dto/WorkingInformationResponseDTO.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class WorkingInformationResponseDTO {
+    private Long id;
     private String position;
     private String educationLevel;
     private String contracType;
