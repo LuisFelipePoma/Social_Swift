5d0b163efa283890d171a747a71414c5c5ebec83
Andre <andrepilcoch@gmail.com>
2024-06-19 18:35:47 -0500

    person service

--- app/src/app/person/dialog-information/dialog-information.component.ts
+++ app/src/app/person/dialog-information/dialog-information.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, inject, model } from '@angular/core'
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog'
 import { PersonResponse } from '../interfaces/person.interface';
+import { PersonService } from '../services/person.service';
 
 @Component({
   selector: 'app-dialog-information',
@@ -10,12 +11,24 @@ import { PersonResponse } from '../interfaces/person.interface';
 export class DialogInformationComponent {
   constructor(
     public dialogRef: MatDialogRef<DialogInformationComponent>, 
-    @Inject(MAT_DIALOG_DATA) public data: { person: PersonResponse }
+    @Inject(MAT_DIALOG_DATA) public data: { person: PersonResponse },
+    private personService: PersonService,
   ) {}
 
   public person = this.data.person;
 
   onClose(): void {
     this.dialogRef.close();
+  }
+
+  moveToBlacklist(id: number) {
+    this.personService.moveToBlacklist(id).subscribe({
+      next: personUpdated => {
+        console.log(personUpdated);
+      },
+      error: error => {
+        console.error('Error al actualizar la persona');
+      }
+    });
   }
 }
